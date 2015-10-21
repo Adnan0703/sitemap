f1632b77029d997ce4c2d274622842100bdf8e5b
Adnan0703 <helloy72@yahoo.com>
2015-10-21 13:06:41 +0500

    testcase for Sitemapclass

--- tests/TestCase/Lib/SitemapTest.php
+++ tests/TestCase/Lib/SitemapTest.php
@@ -8,7 +8,7 @@ use Cake\Core\Configure;
 
 class SitemapTest extends TestCase {
 
-	//public $fixtures = ['core.articles'];
+	public $fixtures = ['app.articles', 'app.posts'];
 
 	public function setUp() 
 	{
@@ -25,6 +25,45 @@ class SitemapTest extends TestCase {
 	 */
 	public function testCreateSitemap() 
 	{
+		set_time_limit(60*10); // if you have lots of records
+		$dirPath = WWW_ROOT . DS . 'sitemaps';
+		$filePath = $dirPath . DS . 'sitemap.xml';
+        if (file_exists($filePath)) {
+            unlink($filePath);
+        }
+		
+		$models = ['Articles', 'Posts'];
+		$this->_setPagesLinks();
+		$sitemap = new \Adnan0703\Sitemap\Lib\Sitemap();
+		$mainSitemap = $sitemap->createSitemap(
+			$dirPath, 
+			'http://cakephp.org/sitemaps/', 
+			$models, 
+			true
+		);
+		$this->assertTrue(file_exists($filePath));
+		$this->assertTrue(filesize($filePath) > 0);
+		// $mainSitemap will be 'http://cakephp.org/sitemaps/sitemap.xml'
+		$this->assertTrue($mainSitemap == 'http://cakephp.org/sitemaps/sitemap.xml');
+	}
+	
+	/**
+	*
+	*/
+	private function _setPagesLinks()
+	{
+		Configure::write('Sitemap.pages', [
+			[
+				'url' => 'http://cakephp.org/pages/home',
+				'priority' => '0.9', 
+				'changefreq' => 'daily'
+			],
+			[
+				'url' => 'http://cakephp.org/pages/about_us',
+				'priority' => '0.9', 
+				'changefreq' => 'daily'
+			],
+		]);
 	}
 
 }
